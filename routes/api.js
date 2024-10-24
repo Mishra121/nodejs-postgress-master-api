ee42dc1a0c8f3de386052cb05aeac8570148a3ca
Vibhu Mishra <vibhumishra@Vibhus-MacBook-Air.local>
2024-10-24 10:09:47 +0530

    feat: news api enpoints and schema added

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController.js";
 import ProfileController from "../controllers/ProfileController.js";
+import NewsController from "../controllers/NewsController.js";
 
 import authMiddleware from "../middleware/Authenticate.js";
 
@@ -12,6 +13,12 @@ router.post("/auth/login", AuthController.login);
 
 router.get("/profile", authMiddleware, ProfileController.index);
 router.put("/profile/:id", authMiddleware, ProfileController.update);
+
+router.get("/news", NewsController.index);
+router.post("/news", authMiddleware, NewsController.store);
+router.get("/news/:id", NewsController.show);
+router.put("/news/:id", authMiddleware, NewsController.update);
+router.delete("/news/:id", authMiddleware, NewsController.destroy);
 
 
 export default router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(fileUpload());
 // app.use(helmet());
 // app.use(cors());
