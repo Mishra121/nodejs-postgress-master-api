7cff7eddf14c02f0e8a1fbe3ba985f575941df67
Vibhu Mishra <vibhumishra@Vibhus-MacBook-Air.local>
2024-10-24 10:17:23 +0530

    feat: security packages and rate limiter

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import "dotenv/config";
 import fileUpload from "express-fileupload";
+import helmet from "helmet";
+import cors from "cors";
+import { limiter } from "./config/ratelimiter.js";
 
 const app = express();
 
@@ -10,9 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(fileUpload());
-// app.use(helmet());
-// app.use(cors());
-// app.use(limiter);
+app.use(helmet());
+app.use(cors());
+app.use(limiter);
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello It's working.." });
